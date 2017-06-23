b97acaf9afd8c3ec83620c3ba3784435a3ee0093
José Roberto Meza Cabrera <robertpro01@gmail.com>
2017-06-22 19:55:31 -0500

    Finished pong app... at least until the tutorial ends

---
+++ main.py
@@ -0,0 +1,10 @@
+from pong.pong import PongApp
+from screen_coordinates.screen_coordinates import ScreenCoordinates
+
+
+def main():
+    PongApp().run()
+    # ScreenCoordinates().run()
+
+if __name__ == "__main__":
+    main()
